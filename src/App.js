4d35463deb30676320650a9573b2b01e82df96d7
Ramanjs <raman.s2662@gmail.com>
2022-09-08 10:58:02 +0530

    fix basename and footer icons

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/landing-page/">
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
